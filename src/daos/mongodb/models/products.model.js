ec648c3348ba6bbfb04acf2920f8ff340a5e8663
jlasheras <juanma_lash@hotmail.com>
2024-07-18 11:05:27 -0300

    reestructura de nuestro servidor

---
+++ src/daos/mongodb/models/products.model.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String, 
+    required: true
+  },
+  code: {
+    type:String, 
+    unique: true, 
+    required: true
+  },
+  price: {
+    type: Number, 
+    required: true
+  },
+  status: {
+    type: Boolean,
+    required: true
+  },
+  stock: {
+    type: Number, 
+    required: true
+  },
+  category: {
+    type: String, 
+    required: true
+  },
+  thumbnails: {
+    type: [String]
+}
+});
+
+export const ProductModel = model("products", productSchema);
